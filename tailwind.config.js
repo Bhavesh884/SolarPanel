8a0c42d86c8933fa7425a54cb4b55074c8405e01
bhavesh884 <bhaveshbhanusalip884@gmail.com>
2024-07-22 00:37:06 +0530

    solar2

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,10 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#3361AC",
-        secondary: "#0F2043",
-        tertiary: "#E8C766",
-        mybtn: "#162F65",
+        primary: "#C3E4E9",
+        secondary: "#222222",
+        tertiary: "#033653",
+        mybtn: "#033653",
         danger: "#e3342f",
         mybg: "#F5F5F5",
       },
